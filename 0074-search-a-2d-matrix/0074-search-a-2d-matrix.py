d1df07acc554117cca687b02a7ebdaf1716c039a
mohdayan311006-source <mohdayan311006@gmail.com>
2026-09-15 23:09:27 +0530

    Time: 0 ms (100%), Space: 12.6 MB (68.61%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def searchMatrix(self, matrix, target):
+        low=0
+        high=len(matrix)*len(matrix[0])-1
+        while low<=high:
+            guess=(low+high)//2
+            row=guess//len(matrix[0])
+            col=guess%len(matrix[0])
+            if matrix[row][col]==target:
+                return True
+            elif matrix[row][col]>target:
+                high=guess-1
+            else:
+                low=guess+1
+        return False
+            
+
+        
+            
+                
+
+        
+        
